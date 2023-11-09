af6102a0f703ce5738e12850eae89505cb32ba49
unknown <admin@pnvl170.pn.com.vn>
2023-11-09 15:32:59 +0700

    finish CRUD NextJS

--- src/component/app.footer.tsx
+++ src/component/app.footer.tsx
@@ -1,16 +1,15 @@
-const  Footer =() =>{
-    return (
-      <div
-        style={{
-          background: "#ddd",
-          textAlign: "center",
-          padding: "5px 0",
-        }}
-      >
-        Khóa học của Đi
-      </div>
-    );
-
-}
+const Footer = () => {
+  return (
+    <div
+      style={{
+        background: "#ddd",
+        textAlign: "center",
+        padding: "5px 0",
+      }}
+    >
+      Khóa học của Đi
+    </div>
+  );
+};
 
 export default Footer;
